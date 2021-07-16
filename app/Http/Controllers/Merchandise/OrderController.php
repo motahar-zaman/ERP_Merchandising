29e8bdfc706b7b12153a322e5bbc71615aa5510a
Zaman <motaharz95@gmail.com>
2021-07-16 10:40:38 +0600

    create controller methods to show element-add UI

--- app/Http/Controllers/Merchandise/OrderController.php
+++ app/Http/Controllers/Merchandise/OrderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Merchandise;
 
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
@@ -12,5 +13,13 @@ class OrderController extends Controller
         return view('merchandising/order/create-order');
     }
 
-    public function createOrder(){}
+    public function createOrder(Request $request){
+        $data = $request->all();
+        $id = 7;
+        return redirect()->route('order-element-ui', ['orderId' => $id]);
+    }
+
+    public function elements($orderId){
+        return view('merchandising/order/create-elements')-> with('orderId', $orderId);
+    }
 }
