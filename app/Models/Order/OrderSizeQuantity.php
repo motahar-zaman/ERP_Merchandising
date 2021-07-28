3be893de42aad52a163a0b7a32a4f5baa4d06ead
Zaman <motaharz95@gmail.com>
2021-07-28 18:27:55 +0600

    Create OrderSizeQuantity model to save and access different size and quantity of a order

---
+++ app/Models/Order/OrderSizeQuantity.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Models\Order;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrderSizeQuantity extends Model
+{
+    protected $table = 'order_size_quantity';
+    protected $primaryKey = 'id';
+}
