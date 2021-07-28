50e513b8e24d5772d0b8d8babea71be231bb3e69
Zaman <motaharz95@gmail.com>
2021-07-28 18:28:59 +0600

    Controller works to store order related data

--- app/Http/Controllers/Merchandise/OrderController.php
+++ app/Http/Controllers/Merchandise/OrderController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Merchandise;
 
 
 use App\Http\Controllers\Controller;
+use App\Models\Order\Order;
+use App\Models\Order\OrderSizeQuantity;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
@@ -15,8 +17,39 @@ class OrderController extends Controller
 
     public function createOrder(Request $request){
         $data = $request->all();
-        $id = 7;
-        return redirect()->route('order-element-ui', ['orderId' => $id]);
+        $orderQuantity = 0;
+        foreach ($data["quantity"] as $quantity){
+            $orderQuantity += $quantity;
+        }
+
+        $order = new Order();
+
+        $order->order_no = $data["order_no"];
+        $order->order_name = $data["description"];
+        $order->style_name = $data["style_name"];
+        $order->buyer_id = $data["buyer"];
+        $order->delivery_date = $data["delivery_date"];
+        $order->quantity = $orderQuantity;
+        $order->status = 1;
+        $order->created_at = date("Y-m-d H:i:s");
+        $order->updated_at = date("Y-m-d H:i:s");
+
+        $order->save();
+
+        foreach ($data["size"] as $index => $sizeName){
+            $size = new OrderSizeQuantity();
+
+            $size->order_id = $order->id;
+            $size->size_name = $sizeName;
+            $size->quantity = $data["quantity"][$index] + 0;
+            $size->status = 1;
+            $size->created_at = date("Y-m-d H:i:s");
+            $size->updated_at = date("Y-m-d H:i:s");
+
+            $size->save();
+        }
+
+        return redirect()->route('order-element-ui', ['orderId' => $order->id]);
     }
 
     public function elements($orderId){
