71033373c4ea229ead25d82957b0b5cbc2ff2a63
Zaman <motaharz95@gmail.com>
2021-07-28 18:26:52 +0600

    Create Order model to save and access order data

---
+++ app/Models/Order/Order.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Models\Order;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $table = 'order';
+    protected $primaryKey = 'id';
+}
