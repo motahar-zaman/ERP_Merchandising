21d5c83645478ecc3c9085dfcfb193b9f71515a8
Zaman <motaharz95@gmail.com>
2021-08-17 13:47:53 +0600

    remove form validation error message showing together at the top of the page

--- resources/views/error/message.blade.php
+++ resources/views/error/message.blade.php
@@ -9,14 +9,3 @@
 	<a style="cursor: pointer;" class="close" data-dismiss="alert">×</a>{!!Session::get('error')!!}
 </div>
 @endif
-
-@if (count($errors->all()) > 0)
-    <div class="alert alert-danger bg-danger">
-    	<a style="cursor: pointer;" class="close" data-dismiss="alert">×</a>
-        {{-- <ul> --}}
-            @foreach ($errors->all() as $error)
-                {{ $error }}<br>
-            @endforeach
-        {{-- </ul> --}}
-    </div>
-@endif
