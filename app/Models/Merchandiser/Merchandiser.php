483d58a750e2bcc9d0f69051f11875e0f06a4ad5
Zaman <motaharz95@gmail.com>
2021-08-19 13:10:03 +0600

    create model to store and access merchandiser information

---
+++ app/Models/Merchandiser/Merchandiser.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Merchandiser;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Merchandiser extends Model
+{
+    protected $table = 'merchandiser';
+    protected $primaryKey = 'id';
+}
